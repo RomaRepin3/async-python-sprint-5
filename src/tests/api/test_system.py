cbbb2ac6bb18cb615be501b11ce7ebaf4c932b68
r.repin <r.repin@keyauto.ru>
2024-06-02 19:06:10 +0300

    [fix] now urls in tests to form automatically

--- src/tests/api/test_system.py
+++ src/tests/api/test_system.py
@@ -1,9 +1,12 @@
 from http import HTTPStatus
 
+from main import app
 
-def test_ping(client):
-    response = client.get('/api/ping')
-    assert response.status_code == HTTPStatus.OK, response.text
-    assert response.json()['db'] is not None, response.text
-    assert response.json()['redis'] is not None, response.text
-    assert response.json()['s3'] is not None, response.text
+
+class TestSystem:
+    async def test_ping(self, client):
+        response = await client.get(app.url_path_for('ping'))
+        assert response.status_code == HTTPStatus.OK, response.text
+        assert response.json()['db'] is not None, response.text
+        assert response.json()['redis'] is not None, response.text
+        assert response.json()['s3'] is not None, response.text
